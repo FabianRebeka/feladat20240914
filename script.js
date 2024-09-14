21a669c52b82e61be4cfe5d7913f3da159515b1b
FábiánRebeka <rebuus11@gmail.com>
2024-09-14 10:18:13 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+/*
+- Keszits BS tablat amiben az epizodok alabbi adatai jelennek meg:
+    - ID
+    - Name
+    - Air_date
+    - Episode
+    - Characters (nem kell megjeleniteni oket, hanem az epizodban szereplo karakterek darabszama kell)
+- API url: https://rickandmortyapi.com/api/episode
+*/
